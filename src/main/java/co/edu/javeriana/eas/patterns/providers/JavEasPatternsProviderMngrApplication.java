e6284669ced022cea3a8dd35b79eb3f2d144be20
ingAndresMartinez <ing.andresmartinezcobos@gmail.com>
2020-04-05 15:22:41 -0500

    Add java project

---
+++ src/main/java/co/edu/javeriana/eas/patterns/providers/JavEasPatternsProviderMngrApplication.java
@@ -0,0 +1,13 @@
+package co.edu.javeriana.eas.patterns.providers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavEasPatternsProviderMngrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavEasPatternsProviderMngrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/edu/javeriana/eas/patterns/providers/JavEasPatternsProviderMngrApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.javeriana.eas.patterns.providers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavEasPatternsProviderMngrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
